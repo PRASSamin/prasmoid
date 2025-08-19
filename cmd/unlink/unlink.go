b89710d12bb3eea20e8b728aa7c2737c8c19e7dd
PRASSamin <prassamin@gmail.com>
2025-08-19 20:10:06 +0600

    test: increase unlink & uninstall command coverage rate to 100%

--- cmd/unlink/unlink.go
+++ cmd/unlink/unlink.go
@@ -4,27 +4,16 @@ Copyright © 2025 PRAS
 package unlink
 
 import (
-	"os"
+	"fmt"
 
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 
 	"github.com/PRASSamin/prasmoid/cmd"
-	"github.com/PRASSamin/prasmoid/utils"
 )
 
 func init() {
 	cmd.RootCmd.AddCommand(UnlinkCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// UnlinkCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// UnlinkCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // UnlinkCmd represents the unlink command
@@ -33,16 +22,15 @@ var UnlinkCmd = &cobra.Command{
 	Short: "Unlink development plasmoid from the system",
 	Long:  "Remove the symlink linking the development plasmoid from the system directories.",
 	Run: func(cmd *cobra.Command, args []string) {
-		dest, err := utils.GetDevDest()
+		dest, err := utilsGetDevDest()
 
 		if err != nil {
-			color.Red(err.Error())
+			fmt.Println(color.RedString(err.Error()))
 			return
 		}
 
 		// Remove if exists
-		_ = os.Remove(dest)
-		_ = os.RemoveAll(dest)
-		color.Green("Plasmoid unlinked successfully.")
+		_ = osRemoveAll(dest)
+		fmt.Println(color.GreenString("Plasmoid unlinked successfully."))
 	},
 }
--- cmd/unlink/unlink_test.go
+++ cmd/unlink/unlink_test.go
@@ -1,11 +1,16 @@
 package unlink
 
 import (
+	"bytes"
+	"errors"
+	"io"
 	"os"
 	"testing"
 
 	"github.com/PRASSamin/prasmoid/tests"
 	"github.com/PRASSamin/prasmoid/utils"
+	"github.com/fatih/color"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLinkAndUnlink(t *testing.T) {
@@ -24,5 +29,23 @@ func TestLinkAndUnlink(t *testing.T) {
 		if _, err := os.Lstat(dest); err == nil {
 			t.Errorf("Expected symlink '%s' to be removed, but it still exists", dest)
 		}
+	})
+	
+	t.Run("failed to get destination", func(t *testing.T) {
+		orgUtilsGetDevDest := utilsGetDevDest
+		utilsGetDevDest = func() (string, error) { return "", errors.New("getdest error") }
+		defer func() { utilsGetDevDest = orgUtilsGetDevDest }()
+
+		orgStdout := os.Stdout
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+		color.Output = w
+		UnlinkCmd.Run(nil, []string{})
+		_ = w.Close()
+
+		var buf bytes.Buffer
+		_, _ = io.Copy(&buf, r)
+		os.Stdout = orgStdout
+		assert.Contains(t, buf.String(), "getdest error")
 	})
 }
